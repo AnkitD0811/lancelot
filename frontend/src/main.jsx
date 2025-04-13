454d0cb55dea59f671d5ecdbfc8b7e141566cb67
Yash Mathur <yashm.devs@gmail.com>
2025-04-13 14:49:20 +0530

    revert to only navbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
 
 // CSS
@@ -13,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
+      {/* <Provider store={store}> */}
         <App />
-      </Provider>
+      {/* </Provider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
--- frontend/src/pages/LiveDashboard.jsx
+++ frontend/src/pages/LiveDashboard.jsx
@@ -2,19 +2,14 @@
 
 // Components
 import Navbar from "../components/Navbar";
-import TrackerPanel from "../components/TrackerPanel";
-import MapPanel from "../components/MapPanel";
-import DataGraph from "../components/DataGraph";
+
 
 function LiveDashboard()
 {
     return <div style={ { height: "100%", width: "100%"}}>
 
         <Navbar />  
-        <TrackerPanel />
-        <MapPanel />
 
-        <DataGraph />
 
     </div>
 }
